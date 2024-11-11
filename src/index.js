9b0177b81e311b19452cf7b8d34ad3340816bcb0
Mallikarjun <mallikarjuna.vedts@gmail.com>
2024-11-11 12:24:52 +0530

    commit by mallikarjun

--- src/index.js
+++ src/index.js
@@ -5,6 +5,9 @@ import App from './App';
 import reportWebVitals from './reportWebVitals';
 import { BrowserRouter } from "react-router-dom";
 
+window.$ = window.jQuery = require("jquery");
+require("bootstrap");
+
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
   <BrowserRouter>
