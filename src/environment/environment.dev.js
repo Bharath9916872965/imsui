53b347d0d7efbffc96645657cbf46dacb22570cd
Anil Kumar <anil.vedts@gmail.com>
2025-01-29 17:43:43 +0530

    commit by Anil

--- src/environment/environment.dev.js
+++ src/environment/environment.dev.js
@@ -1,11 +1,12 @@
 // src/environment/environment.dev.js
 const environment = {
-  API_URL: "http://192.168.1.22:4578/ims_api/",
+  API_URL: "http://192.168.1.15:4578/ims_api/",
   GRANT_TYPE : 'password',
-  CLIENT_ID : 'ims-vts22',
+  CLIENT_ID : 'ims-vts15',
   CLIENT_SECRET : 'HqWpEDmsZFTZQKMbQsFKPQxvfPtOjtGZ',
   TOKEN_URL : 'http://192.168.1.15:9090/realms/DRDONEW/protocol/openid-connect/token',
   MOM_URL : 'http://10.128.19.42:8080/pmslrde/CommitteeMinutesViewAllDownloadPdf.htm',
+  REFRESH_GRANT_TYPE : 'refresh_token'
   // Add other development-specific configurations here
 };
 
